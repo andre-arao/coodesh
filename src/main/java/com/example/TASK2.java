de5f88a593decce2ca4c123570f1ae1e8b7db443
andre-arao <andre.arao@fazenda.rj.gov.br>
2024-04-23 10:59:16 -0300

    TASK 2 finalizada

---
+++ src/main/java/com/example/TASK2.java
@@ -0,0 +1,42 @@
+package com.example;
+
+/**
+ * Task here is to write a list. Each element must know the element before and
+ * after it. Print out your list and them remove the element in the middle of
+ * the list. Print out again.
+ *
+ */
+
+
+public class TASK2 {
+
+    public static void main(String[] args) {
+        ListaDuplamenteEncadeada<String> minhaListaencadeada = new ListaDuplamenteEncadeada<>();
+
+        minhaListaencadeada.add("001");
+        minhaListaencadeada.add("002");
+        minhaListaencadeada.add("003");
+        minhaListaencadeada.add("004");
+        minhaListaencadeada.add("005");
+        minhaListaencadeada.add("006");
+        minhaListaencadeada.add("007");
+
+        System.out.println(minhaListaencadeada);
+        System.out.println("O conteudo da lista na posição 3 é o: " + minhaListaencadeada.get(3));
+
+        System.out.println();
+
+        minhaListaencadeada.remove(3);
+        System.out.println(minhaListaencadeada);
+        System.out.println("O conteudo da lista na posição 3 é o: " + minhaListaencadeada.get(3));
+
+        minhaListaencadeada.add(3, "777");
+
+        System.out.println();
+
+        System.out.println(minhaListaencadeada);
+        System.out.println("O conteudo da lista na posição 3 é o: " + minhaListaencadeada.get(3));
+
+    }
+
+}
